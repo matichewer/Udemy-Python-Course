be3bc677e07cd07482d92d91f055d94fc5048de8
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-02-06 23:44:45 -0300

    Add app11: hotel booking

---
+++ app11_hotel-booking/main.py
@@ -0,0 +1,88 @@
+import pandas
+
+df = pandas.read_csv("hotels.csv", dtype={"id": str})
+df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
+df_cards_security = pandas.read_csv("card_security.csv", dtype=str)
+
+class Hotel:
+
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
+
+    def book(self):
+        """Book a hotel by changing its availability to not available."""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
+
+    def available(self):
+        """Check if the hotel is available or not."""
+        available = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if available == "yes":
+            return True
+        else:
+            return False
+
+
+class Ticket:
+
+    def __init__(self, customer_name, hotel):
+        self.customer_name = customer_name
+        self.hotel = hotel
+
+    def generate(self):
+        content = f"""
+        Thank you for your reservation!
+        Here are your booking data:
+        Name: {self.customer_name}
+        Hotel: {self.hotel.name}
+        """
+        return content
+
+
+class CreditCard:
+
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {
+            "number": self.number,
+            "expiration": expiration,
+            "cvc": cvc,
+            "holder": holder,
+        }
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
+class SecureCreditCard(CreditCard):
+    
+    def authenticate(self, given_password):
+        password = df_cards_security.loc[df_cards_security["number"] == self.number, "password"].squeeze()
+        if password == given_password:
+            return True
+        else:
+            return False
+
+
+print(df)
+hotel_ID = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_ID)
+
+if hotel.available():
+    credit_card = SecureCreditCard(number="1234567890123456")
+    if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
+        if credit_card.authenticate(given_password="mypass"):
+            hotel.book()
+            name = input("Enter your name: ")
+            ticket = Ticket(customer_name=name, hotel=hotel)
+            print(ticket.generate())
+        else:
+            print("Invalid password for the credit card")            
+    else:
+        print("Invalid credit card")
+else:
+    print("Hotel is not free")
