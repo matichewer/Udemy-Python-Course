911312dae7c763c309fcff9e3bbf09f114ca8212
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-02-08 11:53:38 -0300

    Add app13: student managment system with SQLite

---
+++ app13_student-management-system/example.py
@@ -0,0 +1,48 @@
+from PyQt6.QtWidgets import QApplication, QPushButton, QLabel, QWidget \
+    , QLineEdit, QGridLayout
+
+import sys
+from datetime import datetime
+
+class AgeCalculator(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Age Calculator")
+        grid = QGridLayout()
+        
+        # Create widgets
+        name_label = QLabel("Name: ")
+        self.name_label_edit = QLineEdit()
+        
+        date_birth_label = QLabel("Date of Birth (MM/DD/YYYY): ")
+        self.date_birth_line_edit = QLineEdit()  
+        
+        calculate_buttom = QPushButton("Calculate")
+        calculate_buttom.clicked.connect(self.calculate_age)
+        self.output_label = QLabel("")
+        
+        
+        # Add widgets to grid layout
+        grid.addWidget(name_label, 0, 0)
+        grid.addWidget(self.name_label_edit, 0, 1)
+        grid.addWidget(date_birth_label, 1, 0)
+        grid.addWidget(self.date_birth_line_edit, 1, 1)
+        grid.addWidget(calculate_buttom, 2, 0, 1, 2)
+        grid.addWidget(self.output_label, 3, 0, 1, 2)
+        
+        self.setLayout(grid)
+        
+    def calculate_age(self):
+        current_year = datetime.now().year
+        print(current_year)
+        date_of_birth = self.date_birth_line_edit.text()
+        print(date_of_birth)
+        year_of_birth = datetime.strptime(date_of_birth, "%m/%d/%Y").date().year
+        age = current_year - year_of_birth
+        self.output_label.setText(f"{self.name_label_edit.text()} is {age} years old.")
+    
+    
+app = QApplication(sys.argv)
+age_calculator = AgeCalculator()
+age_calculator.show()
+sys.exit(app.exec())
