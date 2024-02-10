812352568ec8d466c192040dabbe0a1a9344bcc6
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-02-10 11:37:49 -0300

    app17 django form: finish project

--- app17_django-form/job_application/admin.py
+++ app17_django-form/job_application/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Form
 
 # Register your models here.
+
+
+class FormAdmin(admin.ModelAdmin):
+    # campos a mostrar
+    list_display = ("first_name", "last_name", "email", "date", "occupation")
+    
+    # busqueda
+    search_fields = ("first_name", "last_name", "email", "date", "occupation")
+    
+    # filtros
+    list_filter = ("date", "occupation")
+    
+    # ordenando en reversa
+    ordering = ("-first_name", )
+    
+
+admin.site.register(Form, FormAdmin)
+
