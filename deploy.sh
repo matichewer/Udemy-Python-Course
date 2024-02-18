6aa95a22c4c3d809615edc63f75c728f5f494e02
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-02-18 12:06:02 -0300

    optimize deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
+THIS_PATH="/home/wecher/02-disco/01-projects/Udemy-Python-Course"
+LOG_FILE="${THIS_PATH}/deploy.log"
+
+IMAGE_NAME="python-course"
+CONTAINER_NAME="web-apps"
+FULL_NAME="${IMAGE_NAME}:${CONTAINER_NAME}"
+
+
+my-echo(){
+    ############################################
+    ############### LOG COLUMNS ################
+    ####### Date,Time,Status,Description #######
+    ############################################
+    echo "$(date -I),$(date +"%T"),$1,$2" >> "${LOG_FILE}"
+    telegram "$1: $2"
+}
+
+
+
+# Voy a la carpeta del proyecto
+cd "${THIS_PATH}"
+
 # Notificar
-telegram "Start Deploy: python-course"
+my-echo "OK" "Deploy start ${FULL_NAME}"
 
 # Actualizar el código
 git pull
 
 # Reconstruir las imagenes que sean necesarias
-docker compose up --build -d
+docker compose up --build --detach
 
 # Notificar
-telegram "End Deploy: python-course"
+my-echo "OK" "Deploy end ${FULL_NAME}"
+
